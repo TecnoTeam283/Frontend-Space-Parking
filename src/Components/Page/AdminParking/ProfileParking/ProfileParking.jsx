1fa76078b20df19454de462813b844388c0d4650
Miguel Ibarra <miguelgoo205@gmail.com>
2023-06-05 15:38:52 -0500

    profile.M.I

--- src/Components/Page/AdminParking/ProfileParking/ProfileParking.jsx
+++ src/Components/Page/AdminParking/ProfileParking/ProfileParking.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { Logo } from '../../../UI/Logo/Logo'
+import { UserDataContext } from '../../Context/UserDataProvider'
 export const ProfileParking = () => {
+
+  const {userDataParking} = useContext(UserDataContext);
   return (
-    <div>ProfileParking</div>
+    <div>
+      <header className='headerParking'>
+        <Logo to='/HomeParking' idLogo='logoHomeUser' />
+        <h3 id='nameUser'>{userDataParking?.nameParking}</h3>
+        <i className='icon-bell'></i>
+        <div className='contIcon'>
+          <i className='icon-user'></i>
+        </div>
+        {/* {showDiv && (
+          <div style={{ display: 'block' }} className='optionsUser'>
+            <Link className='linkOptions' to='/profileParking'>Perfil</Link>
+            <hr />
+            <Link className='linkOptions' to='/'>Cerrar Sesión</Link>
+          </div>
+        )
+        } */}
+      </header>
+
+
+    </div>
   )
 }
